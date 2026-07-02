0fe2af3a1c22ae1da3bc1342ea8bdac546f45f68
bizyutyu <bizyuness.takakura@gmail.com>
2026-07-02 22:41:32 +0900

    chore: add elm-review-unused rules and enable review job
    - Add jfmengels/elm-review-unused 1.1.30 to review/elm.json
    - Configure NoUnused.Dependencies/Parameters/Patterns/Variables rules
    - Ignore .elm-land/ for parameter/pattern/variable rules (generated code)
    - Remove continue-on-error: true from CI review job
    
    Closes #9

--- review/src/ReviewConfig.elm
+++ review/src/ReviewConfig.elm
@@ -1,8 +1,19 @@
 module ReviewConfig exposing (config)
 
-import Review.Rule exposing (Rule)
+import NoUnused.Dependencies
+import NoUnused.Parameters
+import NoUnused.Patterns
+import NoUnused.Variables
+import Review.Rule as Rule exposing (Rule)
 
 
 config : List Rule
 config =
-    []
+    [ NoUnused.Dependencies.rule
+    , NoUnused.Parameters.rule
+        |> Rule.ignoreErrorsForDirectories [ ".elm-land" ]
+    , NoUnused.Patterns.rule
+        |> Rule.ignoreErrorsForDirectories [ ".elm-land" ]
+    , NoUnused.Variables.rule
+        |> Rule.ignoreErrorsForDirectories [ ".elm-land" ]
+    ]
