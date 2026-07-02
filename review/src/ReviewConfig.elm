261c49a2c4a162e800aac99a763c944d2a14e1c4
bizyutyu <bizyuness.takakura@gmail.com>
2026-07-02 22:47:31 +0900

    fix: remove NoUnused.Dependencies to avoid false positives
    .elm-land/ is in .gitignore so elm-review skips generated Main.elm
    which imports elm/browser, elm/json, elm/url. The rule wrongly flags
    them as unused.

--- review/src/ReviewConfig.elm
+++ review/src/ReviewConfig.elm
@@ -1,6 +1,5 @@
 module ReviewConfig exposing (config)
 
-import NoUnused.Dependencies
 import NoUnused.Parameters
 import NoUnused.Patterns
 import NoUnused.Variables
@@ -9,8 +8,10 @@ import Review.Rule as Rule exposing (Rule)
 
 config : List Rule
 config =
-    [ NoUnused.Dependencies.rule
-    , NoUnused.Parameters.rule
+    -- NoUnused.Dependencies は除外: elm-land 生成ファイル (.elm-land/) が
+    -- .gitignore に載っているため elm-review がスキャンせず、
+    -- Browser/Json/Url 等が偽陽性の「未使用」として報告される
+    [ NoUnused.Parameters.rule
         |> Rule.ignoreErrorsForDirectories [ ".elm-land" ]
     , NoUnused.Patterns.rule
         |> Rule.ignoreErrorsForDirectories [ ".elm-land" ]
