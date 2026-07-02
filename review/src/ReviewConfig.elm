004dd1495a00980f94eafe5e3d96473054a0222b
bizyutyu <bizyuness.takakura@gmail.com>
2026-07-02 22:55:25 +0900

    chore: add NoUnused.CustomTypeConstructors/Args rules

--- review/src/ReviewConfig.elm
+++ review/src/ReviewConfig.elm
@@ -1,5 +1,7 @@
 module ReviewConfig exposing (config)
 
+import NoUnused.CustomTypeConstructorArgs
+import NoUnused.CustomTypeConstructors
 import NoUnused.Parameters
 import NoUnused.Patterns
 import NoUnused.Variables
@@ -8,10 +10,14 @@ import Review.Rule as Rule exposing (Rule)
 
 config : List Rule
 config =
-    -- NoUnused.Dependencies は除外: elm-land 生成ファイル (.elm-land/) が
-    -- .gitignore に載っているため elm-review がスキャンせず、
-    -- Browser/Json/Url 等が偽陽性の「未使用」として報告される
-    [ NoUnused.Parameters.rule
+    -- NoUnused.Dependencies/Modules/Exports は除外:
+    -- .elm-land/ が .gitignore に載り elm-review のスキャン対象外になるため、
+    -- それらのファイルに依存する src/ 側の定義が誤って「未使用」と報告される
+    [ NoUnused.CustomTypeConstructorArgs.rule
+        |> Rule.ignoreErrorsForDirectories [ ".elm-land" ]
+    , NoUnused.CustomTypeConstructors.rule []
+        |> Rule.ignoreErrorsForDirectories [ ".elm-land" ]
+    , NoUnused.Parameters.rule
         |> Rule.ignoreErrorsForDirectories [ ".elm-land" ]
     , NoUnused.Patterns.rule
         |> Rule.ignoreErrorsForDirectories [ ".elm-land" ]
